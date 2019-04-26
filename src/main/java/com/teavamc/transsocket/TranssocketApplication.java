ade0b7b8cce3188e70cf922f6e523dcac3970b80
teavamc <teavamc@163.com>
2019-04-26 16:45:14 +0800

    first commit

---
+++ src/main/java/com/teavamc/transsocket/TranssocketApplication.java
@@ -0,0 +1,13 @@
+package com.teavamc.transsocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TranssocketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TranssocketApplication.class, args);
+    }
+
+}
